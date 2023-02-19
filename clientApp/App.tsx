6b8116e0e7ed2b235b1733fb50300a8a4b63a8bf
Medul_Hasan <medul.techvill@gmail.com>
2023-02-19 21:08:23 +0600

    project setup

---
+++ clientApp/App.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Video Streaming App</Text>
+    </View>
+  );
+};
+
+export default App;
